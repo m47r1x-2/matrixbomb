7f097ca3939d268c3e470b141da9888b5289f42a
M47R1X <85056526+m47r1x-2@users.noreply.github.com>
2021-06-26 22:42:11 +0530

    Add files via upload

--- matrixbomb.sh
+++ matrixbomb.sh
@@ -42,7 +42,7 @@ banner() {
     echo -e "\e[1;32m           Telegram: https://t.me/matrixworld \e[0m"
     echo -e "\e[4;32m   YouTube: https://youtu.be/U_sDn7ifzFM \e[0m"
     echo " "
-    echo "NOTE: Kindly move to the PIP version Of TBomb for more stability."
+    echo "NOTE: Kindly move to the PIP version Of matrixbomb for more stability."
     echo " "
 }
 
